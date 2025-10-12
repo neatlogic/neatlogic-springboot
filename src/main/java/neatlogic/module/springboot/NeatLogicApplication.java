500f01f5ab858553b95253f81bd2d216cf32dfcf
1437892690@qq.com <1437892690@qq.com>
2025-10-12 19:33:44 +0800

    [功能] 升级tomcat-embed
    关联 #[1530586131300352]升级tomcat-embed http://192.168.0.96:8090/demo/rdm.html#/story-detail/939050947543040/939050947543042/1530586131300352

--- src/main/java/neatlogic/module/springboot/NeatLogicApplication.java
+++ src/main/java/neatlogic/module/springboot/NeatLogicApplication.java
@@ -108,15 +108,9 @@ public class NeatLogicApplication {
     -Dnacos.home=192.168.0.10:8848
     -Dnacos.namespace=neatlogic
     -DenableNoSecret=true
-    -Xdebug
-    -Xnoagent
-    -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005
-    -XX:+UseConcMarkSweepGC
-    -XX:+PrintGCDetails
-    -XX:+PrintGCTimeStamps
+    -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005
     -XX:+HeapDumpOnOutOfMemoryError
-    -verbose:gc
-    -Xloggc:/neatlogic/logs/gc.log
+    -Xlog:gc*:file=/neatlogic/logs/gc.log:time,uptime,level,tags
     -Djava.io.tmpdir=/neatlogic/tmp
     -Djava.awt.headless=true
     -DenableNoSecret=true
@@ -126,7 +120,7 @@ public class NeatLogicApplication {
 
     /*
     研发环境启动项目
-    用IntelliJ IDEA启动项目时需要将config目录标记为资源根目录,再设置VM options如下：
+    用IntelliJ IDEA启动项目时需要将config目录标记为资源根目录(右键config目录 → Mark Directory as → Resources Root),再设置VM options如下：
 
     -Dlog4j.home=D:\logs2
     -Dlog4j.priority=trace
@@ -146,6 +140,34 @@ public class NeatLogicApplication {
     -DenableNoSecret=false
     //确保JVM使用UTF-8编码来解释和处理文本数据,否则可能会导致中文乱码
     -Dfile.encoding=UTF-8
+     */
+
+    /*
+    运行NeatLogicApplication.main方法启动Springboot时报错Error running 'NeatLogicApplication' Error running NeatLogicApplication. Command line is too long. Shorten the command line and rerun.
+    ‌错误原因‌
+    1.Windows 命令行长度限制‌
+        Windows 对命令行参数的总长度限制为 8191 字符‌（CMD）或 32767 字符‌（PowerShell）。
+        Spring Boot 项目依赖较多时，生成的类路径（CLASSPATH）可能超过此限制。
+    2.‌触发场景‌
+        项目依赖过多（如大型微服务项目）。
+        使用默认的启动配置（JAR Manifest 模式）。
+    ‌解决方案‌
+    1. ‌修改启动配置（推荐）‌
+    在 IDEA 中调整启动配置的 ‌缩短命令行‌ 选项：
+        打开 Run/Debug Configurations。
+        找到你的 Spring Boot 启动配置（如 NeatLogicApplication）。
+        在 Modify options 中选择 Shorten command line‌。
+        选择以下模式：
+            1.JAR Manifest‌：将类路径写入临时文件（适用于大多数场景）。
+    */
+
+    /*
+    运行NeatLogicApplication.main方法启动Springboot项目时报错ERROR: nacos获取不到配置且本地config.properties获取失败,请确认nacos服务是否正常或config.properties文件是否存在,如果存在,文件权限是否正常
+    错误原因‌
+    neatlogic-springboot/target/classes目录下没有application.properties、config.properties、logback.xml、serverid.conf这些文件
+    解决方法：
+        1.需要将config目录标记为资源根目录(右键config目录 → Mark Directory as → Resources Root)
+        2.将neatlogic-springboot/target目录删除，再运行NeatLogicApplication.main方法启动Springboot项目
      */
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(NeatLogicApplication.class);
