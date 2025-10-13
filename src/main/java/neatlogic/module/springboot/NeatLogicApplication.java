94482e33bf9029b66fd332187972ca4a53c0d6ca
linbangquan <1437892690@qq.com>
2025-10-13 10:22:15 +0000

    !1 [功能] 升级tomcat-embed * [功能] 升级tomcat-embed * [功能] 升级tomcat-embed

--- src/main/java/neatlogic/module/springboot/NeatLogicApplication.java
+++ src/main/java/neatlogic/module/springboot/NeatLogicApplication.java
@@ -89,7 +89,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 )
 public class NeatLogicApplication {
     /*
-    生产环境部署项目
+    Linux环境部署项目
     将neatlogic-springboot模块打成neatlogic-springboot-0.4.0.0-SNAPSHOT.jar，随便放在某个目录下，然后将本模块下的config目录复制到该目录下，如下所示：
     /neatloigc
         neatlogic-springboot-0.4.0.0-SNAPSHOT.jar
@@ -108,15 +108,9 @@ public class NeatLogicApplication {
     -Dnacos.home=192.168.0.10:8848
     -Dnacos.namespace=neatlogic
     -DenableNoSecret=true
-    -Xdebug
-    -Xnoagent
-    -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005
-    -XX:+UseConcMarkSweepGC
-    -XX:+PrintGCDetails
-    -XX:+PrintGCTimeStamps
+    -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005
     -XX:+HeapDumpOnOutOfMemoryError
-    -verbose:gc
-    -Xloggc:/neatlogic/logs/gc.log
+    -Xlog:gc*:file=/neatlogic/logs/gc.log:time,uptime,level,tags
     -Djava.io.tmpdir=/neatlogic/tmp
     -Djava.awt.headless=true
     -DenableNoSecret=true
@@ -125,8 +119,38 @@ public class NeatLogicApplication {
     */
 
     /*
+    Windows环境部署项目
+    将neatlogic-springboot模块打成neatlogic-springboot-0.4.0.0-SNAPSHOT.jar，随便放在某个目录下，然后将本模块下的config目录复制到该目录下，如下所示：
+    /neatloigc
+        neatlogic-springboot-0.4.0.0-SNAPSHOT.jar
+        logs
+        config
+            application.properties
+            config.properties
+            logback.xml
+            serverid.conf
+
+    执行命令时将下面命令转成一行，参数之间用空格隔开
+
+    java
+    "-Dlog4j.home=D:\\neatlogic\\logs"
+    "-Dlog4j.priority=trace"
+    "-Dfile.encoding=UTF-8"
+    "-Dnacos.home=192.168.0.10:8848"
+    "-Dnacos.namespace=neatlogic"
+    "-DenableNoSecret=true"
+    "-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005"
+    "-XX:+HeapDumpOnOutOfMemoryError"
+    "-Xlog:gc*:file=D:\\neatlogic\\logs\\gc.log:time,uptime,level,tags"
+    "-Djava.io.tmpdir=D:\\neatlogic\\tmp" "-Djava.awt.headless=true"
+    "-DenableNoSecret=true"
+    "-Xbootclasspath/a:./config"
+    -jar neatlogic-springboot-0.4.0.0-SNAPSHOT.jar
+    */
+
+    /*
     研发环境启动项目
-    用IntelliJ IDEA启动项目时需要将config目录标记为资源根目录,再设置VM options如下：
+    用IntelliJ IDEA启动项目时需要将config目录标记为资源根目录(右键config目录 → Mark Directory as → Resources Root),再设置VM options如下：
 
     -Dlog4j.home=D:\logs2
     -Dlog4j.priority=trace
@@ -146,6 +170,34 @@ public class NeatLogicApplication {
     -DenableNoSecret=false
     //确保JVM使用UTF-8编码来解释和处理文本数据,否则可能会导致中文乱码
     -Dfile.encoding=UTF-8
+     */
+
+    /*
+    运行NeatLogicApplication.main方法启动Springboot时报错Error running 'NeatLogicApplication' Error running NeatLogicApplication. Command line is too long. Shorten the command line and rerun.
+    ‌错误原因‌
+    1.Windows 命令行长度限制‌
+        Windows 对命令行参数的总长度限制为 8191 字符‌（CMD）或 32767 字符‌（PowerShell）。
+        Spring Boot 项目依赖较多时，生成的类路径（CLASSPATH）可能超过此限制。
+    2.‌触发场景‌
+        项目依赖过多（如大型微服务项目）。
+        使用默认的启动配置（JAR Manifest 模式）。
+    ‌解决方案‌
+    1. ‌修改启动配置（推荐）‌
+    在 IDEA 中调整启动配置的 ‌缩短命令行‌ 选项：
+        打开 Run/Debug Configurations。
+        找到你的 Spring Boot 启动配置（如 NeatLogicApplication）。
+        在 Modify options 中选择 Shorten command line‌。
+        选择以下模式：
+            1.JAR Manifest‌：将类路径写入临时文件（适用于大多数场景）。
+    */
+
+    /*
+    运行NeatLogicApplication.main方法启动Springboot项目时报错ERROR: nacos获取不到配置且本地config.properties获取失败,请确认nacos服务是否正常或config.properties文件是否存在,如果存在,文件权限是否正常
+    错误原因‌
+    neatlogic-springboot/target/classes目录下没有application.properties、config.properties、logback.xml、serverid.conf这些文件
+    解决方法：
+        1.需要将config目录标记为资源根目录(右键config目录 → Mark Directory as → Resources Root)
+        2.将neatlogic-springboot/target目录删除，再运行NeatLogicApplication.main方法启动Springboot项目
      */
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(NeatLogicApplication.class);
